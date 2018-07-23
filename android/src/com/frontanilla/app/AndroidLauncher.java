4e435c4580a0885bc68c0e91075b550e393e0d1f
Luis Frontanilla <luchox95@hotmail.com>
2018-07-23 17:23:44 -0400

    Initial commit

---
+++ android/src/com/frontanilla/app/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.frontanilla.app;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.frontanilla.app.CoreLauncher;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CoreLauncher(), config);
+	}
+}
---
+++ desktop/src/com/frontanilla/app/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.frontanilla.app.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.frontanilla.app.CoreLauncher;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CoreLauncher(), config);
+	}
+}
